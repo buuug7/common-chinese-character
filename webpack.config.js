2523f88b7a0977b31ee1d17fdfe727a30395c1ed
buuug7 <puguohong@gmail.com>
2021-01-18 22:06:10 +0800

    update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 const PUBLIC_PATH = "/common-chinese-character/";
@@ -9,50 +9,47 @@ module.exports = {
   entry: "./src/main.js",
   output: {
     path: path.resolve(__dirname, "docs"),
-    publicPath: PUBLIC_PATH
+    publicPath: PUBLIC_PATH,
   },
   devServer: {
-    contentBase: [path.resolve(__dirname, "docs")]
+    contentBase: [path.resolve(__dirname, "docs")],
   },
 
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public/index.html"),
-      publicPath: PUBLIC_PATH
+      publicPath: PUBLIC_PATH,
     }),
-    new CopyWebpackPlugin(
-      [
+    new CopyPlugin({
+      patterns: [
         {
           from: path.resolve(__dirname, "public"),
-          to: path.resolve(__dirname, "docs")
-        }
+          to: path.resolve(__dirname, "docs"),
+        },
       ],
-      {
-        // ignore: ["index.html"]
-      }
-    ),
-    new VueLoaderPlugin()
+    }),
+    new VueLoaderPlugin(),
   ],
 
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: "vue-loader"
+        loader: "vue-loader",
       },
       {
         test: /\.js$/,
         use: {
           loader: "babel-loader",
           options: {
-            plugins: ["@babel/plugin-syntax-dynamic-import"]
-          }
-        }
+            plugins: ["@babel/plugin-syntax-dynamic-import"],
+          },
+        },
       },
       {
         test: /\.(css|scss)$/,
-        loader: ["vue-style-loader", "css-loader", "sass-loader"]
-      }
-    ]
-  }
+        loader: ["vue-style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
 };
